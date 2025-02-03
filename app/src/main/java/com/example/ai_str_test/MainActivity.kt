2b42f7fa71ed171eb39cce605ea6a3155807186d
Sithara T R <sithararao@Sitharas-MacBook-Pro.local>
2025-02-03 18:15:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ai_str_test/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.ai_str_test
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val recyclerView : RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val dataList = arrayListOf<String>()
+
+        for (i in 1..20) {
+            dataList.add("Item $i")
+        }
+
+        val adapter = MyAdapter(dataList)
+        recyclerView.adapter = adapter
+
+
+    }
+}
+
---
+++ app/src/main/java/com/example/ai_str_test/MyAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.ai_str_test
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter(private val dataList: List<String>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textView: TextView = itemView.findViewById(R.id.textView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.textView.text = dataList[position]
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+}
